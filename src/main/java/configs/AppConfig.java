c2c56c7f518d4f4b19ae9420ca93a328dedd43b3
Dietmar Darimont <dietmar.darimont@gmail.com>
2024-01-20 01:25:10 +0100

    add custom exceptions

--- src/main/java/configs/AppConfig.java
+++ src/main/java/configs/AppConfig.java
@@ -3,6 +3,8 @@ package configs;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+
+import exceptions.ConfigurationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,13 +19,19 @@ public class AppConfig {
     private AppConfig() {
         Properties properties = new Properties();
         try (InputStream input = AppConfig.class.getClassLoader().getResourceAsStream("db.properties")) {
+            if (input == null) {
+                throw new ConfigurationException("Konfigurationsdatei db.properties nicht gefunden");
+            }
             properties.load(input);
             dbUrl = properties.getProperty("db.url");
             dbUser = properties.getProperty("db.user");
             dbPassword = properties.getProperty("db.password");
+            if (dbUrl == null || dbUser == null || dbPassword == null) {
+                throw new ConfigurationException("Erforderliche Datenbankkonfigurationen fehlen in db.properties");
+            }
         } catch (IOException e) {
-            LOGGER.error("Fehler beim Laden der Konfigurationsdatei! {}", e.getMessage());
-            throw new RuntimeException("Konfigurationsfehler", e);
+            LOGGER.error("Fehler beim Laden der Konfigurationsdatei db.properties! {}", e.getMessage());
+            throw new ConfigurationException("Fehler beim Laden der Konfigurationsdatei db.properties", e);
         }
     }
 
