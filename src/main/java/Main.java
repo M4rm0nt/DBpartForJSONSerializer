ffa4a261f77034ffe1d797c24f76fd59066c3a89
Dietmar Darimont <dietmar.darimont@gmail.com>
2024-01-20 01:19:00 +0100

    main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,20 +7,22 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+
         MenschService service = new MenschService();
         JsonSerializer serializer = new JsonSerializer();
 
+        /*
+        Mensch mensch = service.getMenschByName("Alice");
+        String json = serializer.serialize(mensch);
+
+        System.out.println(json);
+        */
+
         List<Mensch> menschen = new ArrayList<>();
         menschen.add(service.getMenschByName("Alice"));
-
-        /*
         menschen.add(service.getMenschByName("Bob"));
         menschen.add(service.getMenschByName("Cedrik"));
-         */
 
-        for (Mensch mensch : menschen) {
-            String json = serializer.serialize(mensch);
-            System.out.println(json);
-        }
+        System.out.println(serializer.serializeList(menschen));
     }
 }
